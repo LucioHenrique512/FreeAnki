80975c6d6631bc70cceccc7b87cb73ed885a8c82
Lúcio Henrique <lucions96@gmail.com>
2021-04-08 11:51:37 -0300

    add theme on styled component

--- src/screens/startpage/styles.ts
+++ src/screens/startpage/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components/native';
+import {Sizes} from '../../commons';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  height: ${Sizes.SCREEN_HEIGHT}px;
+  width: ${Sizes.SCREEN_WIDTH}px;
+  background: ${({theme}) => theme.primary};
+`;
